e0ae4fb50f21f634d1c46ce1839de708fc1cf358
Sebastian Richel <47844429+sebastianrich18@users.noreply.github.com>
2021-07-12 19:05:59 -0400

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,23 +1,16 @@
 users = {
     "sebastian": {
-        "email": "sebastianrichel18@gmail.com",
-        "password": "Lucy418418",
-        "deviceId": "B1arzhnm1VInvZlcTG76olQ6i4de5IMWckLzrPwmu90w9euA41jBR+HYVNSUzmWV"
-    },
-        "victoria": {
-            "email": "mwrichel@roadrunner.com",
-            "password": "Eeyore22!",
-            "deviceId": "dYgDxjNDw6A/HIUuTxWWI79pxG8a36H1i7mxiFvihSk="
-    },
-        "mom": {
-            "email": "melissa.richel@yahoo.com",
-            "password": "Oliver101010",
-            "deviceId": "dYgDxjNDw6Dm2Na2W3W8b5cD4J0OTpG2i7mxiFvihSk="
+        "email": "ICLOUD-EMAIL",
+        "password": "ICLOUD-PASSWORD",
+        "deviceId": "DEVICE_ID"
+    }    "sebastian": {
+        "email": "ICLOUD-EMAIL",
+        "password": "ICLOUD-PASSWORD",
+        "deviceId": "DEVICE_ID"
     }
-}
 
 garage = {
-    "email": "sebastianrichel18@gmail.com",
-    "password": "Oliver101010"
+    "email": "MYQ_EMAIL",
+    "password": "MYQ_PASSWORD"
 }
 
